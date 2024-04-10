7d46e1604564a180d4a3f34cd06ab7d0b7cc7c82
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-04-09 18:39:37 -0600

    .

--- math.software---mvp/src/App.tsx
+++ math.software---mvp/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
                     {
                         "essential": true,
                         "functional": true,
-                        "analytics": true,
+                        "analytical": true,
                         "targeting": true,
                     },
                 ),
